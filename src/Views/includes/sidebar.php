aa4c9a9fcf22ae7d0392e101a87974ad85425124
Christine <christineelise22@gmail.com>
2025-11-27 21:56:38 +0800

    Add sidebar navigation to the views

---
+++ src/Views/includes/sidebar.php
@@ -0,0 +1,17 @@
+<div class="sidebar">
+    <div class="sidebar-header">
+        <h2>Menu</h2>
+    </div>
+
+    <nav class="sidebar-nav">
+        <a href="../Customer/customers.php" class="nav-item">
+            <span class="nav-text">Customers</span>
+        </a>
+    </nav>
+    <nav class="sidebar-nav">
+        <a href="../Order/order.php" class="nav-item">
+            <span class="nav-text">Orders</span>
+        </a>
+    </nav>
+    
+</div>

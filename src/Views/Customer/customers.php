15062b3fba2e5837e33a5d1744aa8a6b78a540fa
Christine <christineelise22@gmail.com>
2025-11-27 21:58:46 +0800

    Replace order.css with style.css in customers.php

--- src/Views/Customer/customers.php
+++ src/Views/Customer/customers.php
@@ -28,7 +28,7 @@ $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/order.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 
 </head>
 <body>
